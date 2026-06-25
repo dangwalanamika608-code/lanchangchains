f23ff95b258f59b95158b09a80fa193f17240d46
dangwalanamika608-code <dangwalanamika608@gmail.com>
2026-06-26 04:11:21 +0530

    Add dynamic summarization tool for research papers
    Implement a dynamic research paper summarization tool using Streamlit and HuggingFace.

---
+++ prompt_dynamic.py
@@ -0,0 +1,42 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+#from langchain_google_genai import ChatGoogleGenerativeAI
+#from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import  PromptTemplate,load_prompt
+load_dotenv()
+llm = HuggingFaceEndpoint(
+     repo_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+)
+
+# 2. Pass it to ChatHuggingFace via the 'llm' keyword argument
+model = ChatHuggingFace(llm=llm)
+st.header('research tool')
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+#template 
+template = load_prompt('template.json')
+
+#fill the placeholders
+
+if st.button('summarise'):
+
+
+    chain = template|model
+    result =  chain.invoke({
+    'paper_input':paper_input,
+    'style_input':style_input,
+    'length_input':length_input
+    }
+
+    )
+    #fill the placeholders
+
+   
+    st.write(result.content)
